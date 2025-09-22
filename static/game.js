d5eaea6ad2f0ccc1c6d37b0cfd0c36cff3627ad1
Zan <99092011+Zanixpy@users.noreply.github.com>
2025-09-22 11:34:05 +0000

    Mon, Sep 22, 2025, 1:34 PM +02:00

--- static/game.js
+++ static/game.js
@@ -9,7 +9,7 @@ async function getAPI(url)
     if (!response.ok)
         throw new Error(`Response status: ${response.status}`)
     const result = await response.json()
-    return result.json()
+    return result
 
   } catch (error){
     console.error(error.message)
